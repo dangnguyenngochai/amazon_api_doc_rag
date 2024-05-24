591eca7219c668b1f95583ecabdf40e53edd9e1f
Dang <dangnguyenngochai.nnhd@gmail.com>
2024-05-24 16:10:05 +0700

    add test

---
+++ test.py
@@ -0,0 +1,14 @@
+# from openai import OpenAI
+
+import os
+
+from embedding import *
+from retrieval_generation import *
+
+# from embeddings_api_docs import test_run
+from retrieval_generation import test_run2
+
+from config import EMB_MODEL
+
+if __name__ == "__main__":
+    test_run2()

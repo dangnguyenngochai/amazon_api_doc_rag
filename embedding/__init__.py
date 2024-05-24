11e66b5bc32904a8081d745173cdaff497421e27
Dang <dangnguyenngochai.nnhd@gmail.com>
2024-05-24 16:11:58 +0700

    fix import error

--- embedding/__init__.py
+++ embedding/__init__.py
@@ -1,3 +1,3 @@
-import api_loader
-import embeddings_api_docs
-from embeddings_api_docs import EncodedApiDocVectorStore
+from . import api_loader
+from . import embeddings_api_docs
+from .embeddings_api_docs import EncodedApiDocVectorStore
